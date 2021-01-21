b94347979462bd1fffc99df60108a46915fa21d0
antonpodkur <anton.podkur@gmail.com>
2021-01-21 17:03:55 +0200

    Added search

--- client/src/Components/App.vue
+++ client/src/Components/App.vue
@@ -4,231 +4,12 @@
     <div class="container p-4">
       <div class="row">
         <div class="col-sm-4 col-md-3">
-          <form>
-            <div class="well pb-4">
-              <div class="row">
-                <div class="col-sm-12">
-                  <div class="input-group">
-                    <input
-                      type="text"
-                      class="form-control"
-                      placeholder="Search products..."
-                    />
-                    <span class="input-group-btn">
-                      <button class="btn btn-primary" type="submit">
-                        <i class="fa fa-search"></i>
-                      </button>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </form>
-
-          <!-- Filter -->
-          <form class="shop__filter">
-            <!-- Price -->
-            <h3 class="headline">
-              <span>Price</span>
-            </h3>
-            <div class="radio">
-              <input
-                type="radio"
-                name="shop-filter__price"
-                id="shop-filter-price_1"
-                value=""
-                checked=""
-              />
-              <label for="shop-filter-price_1">Under $25</label>
-            </div>
-            <div class="radio">
-              <input
-                type="radio"
-                name="shop-filter__price"
-                id="shop-filter-price_2"
-                value=""
-              />
-              <label for="shop-filter-price_2">$25 to $50</label>
-            </div>
-            <div class="radio">
-              <input
-                type="radio"
-                name="shop-filter__price"
-                id="shop-filter-price_3"
-                value=""
-              />
-              <label for="shop-filter-price_3">$50 to $100</label>
-            </div>
-            <div class="radio">
-              <input
-                type="radio"
-                name="shop-filter__price"
-                id="shop-filter-price_4"
-                value="specify"
-              />
-              <label for="shop-filter-price_4">Other (specify)</label>
-            </div>
-            <div class="form-group shop-filter__price">
-              <div class="d-flex">
-                <div class="col-xs-4">
-                  <label for="shop-filter-price_from" class="sr-only"></label>
-                  <input
-                    id="shop-filter-price_from"
-                    type="number"
-                    min="0"
-                    class="form-control"
-                    placeholder="From"
-                    disabled=""
-                  />
-                </div>
-                <div class="col-xs-4">
-                  <label for="shop-filter-price_to" class="sr-only"></label>
-                  <input
-                    id="shop-filter-price_to"
-                    type="number"
-                    min="0"
-                    class="form-control"
-                    placeholder="To"
-                    disabled=""
-                  />
-                </div>
-                <div class="col-xs-4">
-                  <button type="submit" class="btn btn-block btn-default" disabled="">
-                    Go
-                  </button>
-                </div>
-              </div>
-            </div>
-
-            <!-- Checkboxes -->
-            <h3 class="headline">
-              <span>Brand</span>
-            </h3>
-            <div class="checkbox">
-              <input type="checkbox" value="" id="shop-filter-checkbox_1" checked="" />
-              <label for="shop-filter-checkbox_1">Adidas</label>
-            </div>
-            <div class="checkbox">
-              <input type="checkbox" value="" id="shop-filter-checkbox_2" />
-              <label for="shop-filter-checkbox_2">Calvin Klein</label>
-            </div>
-            <div class="checkbox">
-              <input type="checkbox" value="" id="shop-filter-checkbox_3" />
-              <label for="shop-filter-checkbox_3">Columbia</label>
-            </div>
-            <div class="checkbox">
-              <input type="checkbox" value="" id="shop-filter-checkbox_4" />
-              <label for="shop-filter-checkbox_4">Tommy Hilfiger</label>
-            </div>
-            <div class="checkbox">
-              <input type="checkbox" value="" id="shop-filter-checkbox_5" />
-              <label for="shop-filter-checkbox_5">Not specified</label>
-            </div>
-
-            <!-- Radios -->
-            <h3 class="headline">
-              <span>Material</span>
-            </h3>
-            <div class="radio">
-              <input
-                type="radio"
-                name="shop-filter__radio"
-                id="shop-filter-radio_1"
-                value=""
-                checked=""
-              />
-              <label for="shop-filter-radio_1">100% Cotton</label>
-            </div>
-            <div class="radio">
-              <input
-                type="radio"
-                name="shop-filter__radio"
-                id="shop-filter-radio_2"
-                value=""
-              />
-              <label for="shop-filter-radio_2">Bamboo</label>
-            </div>
-            <div class="radio">
-              <input
-                type="radio"
-                name="shop-filter__radio"
-                id="shop-filter-radio_3"
-                value=""
-              />
-              <label for="shop-filter-radio_3">Leather</label>
-            </div>
-            <div class="radio">
-              <input
-                type="radio"
-                name="shop-filter__radio"
-                id="shop-filter-radio_4"
-                value=""
-              />
-              <label for="shop-filter-radio_4">Polyester</label>
-            </div>
-            <div class="radio">
-              <input
-                type="radio"
-                name="shop-filter__radio"
-                id="shop-filter-radio_5"
-                value=""
-              />
-              <label for="shop-filter-radio_5">Not specified</label>
-            </div>
-
-            <!-- Colors -->
-            <h3 class="headline">
-              <span>Colors</span>
-            </h3>
-            <div class="shop-filter__color">
-              <input
-                type="text"
-                id="shop-filter-color_1"
-                value=""
-                data-input-color="black"
-              />
-              <label for="shop-filter-color_1" style="background-color: black"></label>
-            </div>
-            <div class="shop-filter__color">
-              <input
-                type="text"
-                id="shop-filter-color_2"
-                value=""
-                data-input-color="gray"
-              />
-              <label for="shop-filter-color_2" style="background-color: gray"></label>
-            </div>
-            <div class="shop-filter__color">
-              <input
-                type="text"
-                id="shop-filter-color_3"
-                value=""
-                data-input-color="brown"
-              />
-              <label for="shop-filter-color_3" style="background-color: brown"></label>
-            </div>
-            <div class="shop-filter__color">
-              <input
-                type="text"
-                id="shop-filter-color_4"
-                value=""
-                data-input-color="beige"
-              />
-              <label for="shop-filter-color_4" style="background-color: beige"></label>
-            </div>
-            <div class="shop-filter__color">
-              <input
-                type="text"
-                id="shop-filter-color_5"
-                value=""
-                data-input-color="white"
-              />
-              <label for="shop-filter-color_5" style="background-color: white"></label>
-            </div>
-          </form>
+          <Sidebar
+              @search="Search"
+          />
         </div>
 
-        <div class="col-sm-8 col-md-9">
+        <div v-if="products.length" class="col-sm-8 col-md-9">
              <h2 class="text-center">ТОП-3 найдешевші</h2>
           <div class="row" >
             <div
@@ -284,6 +65,10 @@
 
         </div>
 
+        <div v-else>
+          <h3>Nothing has been found</h3>
+        </div>
+
         <div class="col-sm-8 col-md-9">
 
         </div>
@@ -295,17 +80,19 @@
 
 <script>
 import Loader from "./Loader";
+import Sidebar from "@/Components/Sidebar";
 export default {
   name: "App",
   components: {
-    Loader,
+    Loader, Sidebar
   },
   data: () => ({
     products: [],
     isLoading: true,
+    param: ""
   }),
   async mounted() {
-    const responseBefore = await fetch("http://localhost:3333/api/products");
+    const responseBefore = await fetch("/api/products");
     const response = await responseBefore.json();
     this.products = response.products;
     // this.sortProducts();
@@ -324,6 +111,14 @@ export default {
         return 0;
       });
     },
+    async Search(param) {
+      this.isLoading = true;
+      console.log(param)
+      const responseBefore = await fetch(`http://localhost:3333/api/products?searchString=${param}`);
+      const response = await responseBefore.json();
+      this.products = response.products;
+      this.isLoading = false;
+    }
   },
   computed: {
     topProduct() {
--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const grabJson = require('./services/apiService');
 
 app.get('/api/products', async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*')
-    const products = await grabJson();
+    let products;
+    if(req.query.searchString != "")  products = await grabJson(req.query.searchString);
+    else products = await grabJson();
     res.status(200).json({
         products
     });
@@ -13,12 +15,6 @@ app.get('/api/products', async (req, res) => {
 
 app.use('/', express.static('./client/dist'));
 
-(async () => {
-    try {
-        app.listen(port, () => {
-            console.log(`Server started, port: ${port}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-})();
+app.listen(port, () => {
+    console.log(`Server started, port: ${port}`);
+});
