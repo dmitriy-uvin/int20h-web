5993c17053daf79b7650d739890cef90d293b3f6
Dmitriy Uvin <dimauuvin@gmail.com>
2021-01-21 12:13:35 +0200

    Make server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3333;
+const grabJson = require('./services/apiService');
+
+app.get('/api/products', async (req, res) => {
+    const products = await grabJson();
+    res.status(200).json({
+        products
+    });
+});
+
+app.use('/', express.static('./client/dist'));
+
+(async () => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server started, port: ${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})();
