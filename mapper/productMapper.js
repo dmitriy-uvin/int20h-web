2bf01669f8a9b386f12f276d98b3ec0d9bd3889c
Dmitriy Uvin <dimauuvin@gmail.com>
2021-01-21 12:13:25 +0200

    Add product mapper

---
+++ mapper/productMapper.js
@@ -0,0 +1,13 @@
+module.exports = Product => {
+    return {
+        title: Product.title,
+        unit: Product.unit,
+        bundle: Product.bundle,
+        price: Product.price / 100,
+        link: Product.web_url,
+        image: Product.img.s200x200,
+        producer: Product.producer.trademark,
+        weight: Product.weight,
+        pricePerGramm: 0
+    };
+}
