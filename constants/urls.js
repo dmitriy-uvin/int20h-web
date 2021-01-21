175818918025e099cbabb4adb308a98125eab3fc
Dmitriy Uvin <dimauuvin@gmail.com>
2021-01-21 12:14:05 +0200

    Add array with urls

---
+++ constants/urls.js
@@ -0,0 +1,6 @@
+module.exports = [
+    'https://stores-api.zakaz.ua/stores/48246401/products/search/?q=',
+    'https://stores-api.zakaz.ua/stores/48201070/products/search/?q=',
+    'https://stores-api.zakaz.ua/stores/48215611/products/search/?q=',
+    'https://stores-api.zakaz.ua/stores/48280214/products/search/?q='
+];
